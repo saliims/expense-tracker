6e2dd84ccca46c53e6f58965127930c624cd255a
Domo-Salim <saliim.domo@gmail.com>
2024-02-17 23:29:45 +0100

    first client commit

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Expense from "./pages/Expense";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Income from "./pages/Income";
+import AppLayout from "./ui/AppLayout";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="expenses" element={<Expense />} />
+          <Route path="incomes" element={<Income />} />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/styles/GlobalStyles.js
@@ -0,0 +1,61 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  transition: background-color 0.3s, border 0.3s;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  font-family: "Poppins", sans-serif;
+  min-height: 100vh;
+  line-height: 1.5;
+  font-size: 1.6rem;
+}
+
+input,
+button,
+textarea,
+select {
+  font: inherit;
+  color: inherit;
+}
+
+button {
+  cursor: pointer;
+}
+
+*:disabled {
+  cursor: not-allowed;
+}
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+ul {
+  list-style: none;
+}
+
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  overflow-wrap: break-word;
+  hyphens: auto;
+}
+
+`;
+
+export default GlobalStyles;
